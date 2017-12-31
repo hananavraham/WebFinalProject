ccaae3a0aaffe69ca6cd7a6aee582b2861f00092
hananavraham <32907433+hananavraham@users.noreply.github.com>
2017-12-31 17:28:27 +0200

    Add files via upload

---
+++ includes/nav_footer.js
@@ -0,0 +1,6 @@
+             
+var footer = $('<section><ul><li><a href="#">Workout Tracker</a></li><li><a href="#">contact | </a></li> <li><a href="#">Temp Of use</a></li></ul></section><section><ul><li><a href="#"></a></li><li><a href="#"></a></li><li><a href="#"></a></li><li><a href="#"></a></li></ul></section>');
+        
+var nav = $('<ul><li><a href="index.html">HOME</a></li><li><a href="trainers.html">TRAINERS</a></li><li><a href="#">WORKOUT</a></li><li><a href="#">CONTACT</a></li><li><a href="login.html">LOGIN</a></li><li><div class="search-container"><input type="text" placeholder="Search.." name="search"><button type="submit"><i class="fa fa-search"></i></button></div></li></ul>');     
+              
+var header = $('<a href="index.html"><span class="logo1">Workout Tracker</span></a><article>IT DOSEN`T GET EASIER YOU JUST GET BETTER</article>');            
--- includes/script.js
+++ includes/script.js
@@ -1,3 +1,83 @@
 
+var pagePathName;
+var name;
+var programs;
+function init() {		// function that call to all function(addHeader,addNav,addFooter)
+	pagePathName = window.location.pathname;
+    name = pagePathName.substring(pagePathName.lastIndexOf("/") + 1);
+	insertHeader();
+	insertNav();
+    insertFooter();
+    showPopUp();
+    add_program();
+}
 
+programs ={
+	sugget_programs:{
+		program1:"Loss Fat",
+		program2:1,
+		program3:1,
+		program4:1,
+		program5:1,
+		program6:1,
+	}
+}
+
+var main = $('main').css('paddig-top','20px');
+
+var program = $('<div class="card-deck">');
+var suggets_prog = programs.sugget_programs;
+
+function add_program(){
+	for (obj in suggets_prog){
+	var img = $('<img class="rounded mx-auto d-block" width="200px" height="148px">')
+		.attr('src',`images/${obj}.png`);
+	var card = $('<div class="card text-center">')
+		.css('width','200px');
+	var button = $('<a href="#" class="btn btn-dark">choose!</a>').on('click',choose);
+	card.append(img);
+	card.append(
+		$('<div class="card-body">')
+			.append('<h4 class="card-title">' + suggets_prog[obj] + '</h4>')
+			.append('<p class="card-text"><small class="text-muted">')
+			.append(button)
+		)
+	$('main .row').append(card);
+	}
+}
+
+function choose(){
+	var element = $(this).siblings().get(0);
+	var text = $(element).text();
+	console.log('you choose: ',text);
+}
+
+
+function showPopUp(){
+	if (name == "newtrainee2.html"){
+		$("#submit").click(function(){
+			console.log("pop");
+			$('#popup').css('visibility','visible');    
+		});
+		$("#ok").click(function(){
+			$('#popup').css('visibility','hidden');   
+		});
+	}	
+}
+
+function insertHeader(){
+	$("body header").append(header);
+}
+
+
+function insertNav(){
+    if (!(name == "index.html" || name == "login.html")){
+    	return;
+    }
+	$("body nav").append(nav);
+}
+
+function insertFooter(){
+	$("body footer").append(footer);
+}
 
